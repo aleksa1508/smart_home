5dd7b3ab8cc885685b28e22c59c4eb2c3311deeb
aleksa1508 <aleksaarsenic15.08@gmail.com>
2026-04-11 18:58:20 +0200

    Added class libraries for users and devices

---
+++ SmartHome/KucniUredjaji/Uredjaj.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KucniUredjaji
+{
+    public class ResponseDto
+    {
+        public string Message { get; set; } = string.Empty;
+        public Uredjaj Uredjaj { get; set; }
+        public string Function { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public List<Uredjaj> Uredjaji { get; set; }
+    }
+    public class Komanda
+    {
+        public int ID { get; set; }
+        public string Log { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; }
+        public Komanda() { }
+
+        public override string ToString()
+        {
+            return $"{Log}";
+        }
+    }
+    [Serializable]
+    public class Uredjaj
+    {
+        public string Ime { get; set; }                // Ime uređaja
+        public int Port { get; set; }                  // Port na kojem uređaj komunicira
+        public Dictionary<string, string> Funkcije { get; set; } // Funkcije uređaja i njihove vrednosti
+        public List<Komanda> EvidencijaKomandi { get; set; } // Evidencija komandi sa vremenskim oznakama
+        public DateTime PoslednjaPromena { get; private set; }      // Vremenska oznaka poslednje promene
+
+        public List<Uredjaj> uredjaji { get; set; }
+        // Konstruktor
+        public Uredjaj(string ime, int port)
+        {
+            Ime = ime;
+            Port = port;
+            Funkcije = new Dictionary<string, string>();
+            EvidencijaKomandi = new List<Komanda>();
+            PoslednjaPromena = DateTime.Now;
+        }
+        public Uredjaj(string ime, int port, Dictionary<string, string> funkcije)
+        {
+            Ime = ime;
+            Port = port;
+            Funkcije = funkcije;
+            EvidencijaKomandi = new List<Komanda>();
+            PoslednjaPromena = DateTime.Now;
+        }
+
+        public Uredjaj()
+        {
+            uredjaji = new List<Uredjaj> {
+                        new Uredjaj("Svetlo",60001,new Dictionary<string, string>{{ "intenzitet", "70" }, { "stanje", "OFF" }, { "boja crvena", "110" }}),
+                        new Uredjaj("TV",60002,new Dictionary<string, string>{{ "stanje", "OFF" },{ "temperatura", "15" }}),
+                        new Uredjaj("Klima",60003,new Dictionary<string, string>{{ "stanje", "OFF" },{ "temperatura", "15" }}),
+                        new Uredjaj("Door",60004,new Dictionary<string, string>{{ "stanje", "OFF" }})
+            };
+        }
+
+        // Dodavanje ili ažuriranje funkcije uređaja
+        public void AzurirajFunkciju(string funkcija, string vrednost)
+        {
+            if (Funkcije.ContainsKey(funkcija))
+            {
+                Funkcije[funkcija] = vrednost;
+            }
+            else
+            {
+                Funkcije.Add(funkcija, vrednost);
+            }
+
+            // Ažuriraj vremensku oznaku
+            PoslednjaPromena = DateTime.Now;
+
+            // Evidentiraj promenu
+            //EvidencijaKomandi.Add($"[{PoslednjaPromena}] {Ime}: {funkcija} promenjena na {vrednost}");
+            EvidencijaKomandi.Add(new Komanda { ID = EvidencijaKomandi.Count + 1, CreationDate = PoslednjaPromena, Log = $"[{PoslednjaPromena}] {Ime}: {funkcija} promenjena na {vrednost}" });
+        }
+       
+        public List<Uredjaj> SviUredjaji()
+        {
+            return uredjaji;
+        }
+       /* public void AzurirajListu(List<Uredjaj> noviUredjaji)
+        {
+            uredjaji = noviUredjaji;
+        }*/
+
+        public string IspisiSveUredjajeUTabeli(List<Uredjaj> lista)
+        {
+            // Zaglavlje tabele
+            string tabela = string.Format("{0,-15} | {1,-10} | {2,-50}\n", "Ime Uređaja", "Port", "Funkcije");
+            tabela += new string('-', 80) + "\n";
+
+            // Ispis uređaja
+            foreach (var uredjaj in lista)
+            {
+                // Pretvaranje funkcija u format ključ: vrednost
+                string funkcije = string.Join(", ", uredjaj.Funkcije.Select(f => $"{f.Key}: {f.Value}"));
+
+                // Dodavanje uređaja u tabelu
+                tabela += string.Format("{0,-15} | {1,-10} | {2,-50}\n", uredjaj.Ime, uredjaj.Port, funkcije);
+            }
+
+            return tabela;
+        }
+        public string IspisiSveFunkcijeUredjaja()
+        {
+            // Zaglavlje tabele
+            string tabela = string.Format("{0,-15} | {1,-10} | {2,-50}\n", "Ime Uređaja", "Port", "Funkcije");
+            tabela += new string('-', 80) + "\n";
+
+            // Pretvaranje funkcija u format ključ: vrednost
+            string funkcije = string.Join(", ", this.Funkcije.Select(f => $"{f.Key}: {f.Value}"));
+
+            // Dodavanje uređaja u tabelu
+            tabela += string.Format("{0,-15} | {1,-10} | {2,-50}\n", this.Ime, this.Port, funkcije);
+
+
+            return tabela;
+        }
+       
+
+    }
+}
---
+++ SmartHome/UsersLibrary/Korisnici.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsersLibrary
+{
+    [Serializable]
+    public class Korisnici
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public StatusKorisnika StatusPrijave { get; set; }
+        public int DodeljeniPort { get; set; }
+
+        public Korisnici(string ime, string prezime, string korisnickoIme, string lozinka, StatusKorisnika statusPrijave, int dodeljeniPort)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            KorisnickoIme = korisnickoIme;
+            Lozinka = lozinka;
+            StatusPrijave = statusPrijave;
+            DodeljeniPort = dodeljeniPort;
+        }
+
+        public Korisnici()
+        {
+        }
+
+        public Korisnici PretraziKorisnika(List<Korisnici> korisnici, string korisnickoIme, string lozinka)
+        {
+            foreach (var korisnik in korisnici)
+            {
+                if (korisnik.KorisnickoIme == korisnickoIme && korisnik.Lozinka == lozinka)
+                {
+                    korisnik.StatusPrijave = StatusKorisnika.AKTIVAN;
+                    return korisnik;
+                }
+            }
+            return null; // Ako korisnik nije pronađen
+        }
+
+        public void PretragaPorta(List<Korisnici> korisnici, int port)
+        {
+            foreach (var korisnik in korisnici)
+            {
+                if (korisnik.DodeljeniPort == port)
+                {
+                    korisnik.StatusPrijave = StatusKorisnika.NEAKTIVAN;
+                }
+            }
+        }
+
+        public bool PretragaNeaktivnosti(List<Korisnici> korisnici)
+        {
+            foreach (var korisnik in korisnici)
+            {
+                if (korisnik.StatusPrijave == StatusKorisnika.AKTIVAN)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public void IspisKorisnika(List<Korisnici> lista)
+        {
+            Console.WriteLine("--------------------------------------------------------------------------------");
+            Console.WriteLine("| Ime       | Prezime     | Korisničko ime | Prijavljen | Port  | Lozinka      |");
+            Console.WriteLine("--------------------------------------------------------------------------------");
+
+            foreach (var korisnik in lista)
+            {
+                Console.WriteLine($"| {korisnik.Ime.PadRight(10)} | {korisnik.Prezime.PadRight(10)} | {korisnik.KorisnickoIme.PadRight(15)} | {(korisnik.StatusPrijave==StatusKorisnika.AKTIVAN ? "DA " : "NE ").PadRight(9)} | {korisnik.DodeljeniPort.ToString().PadRight(5)} | {korisnik.Lozinka.PadRight(12)} |");
+            }
+            Console.WriteLine("--------------------------------------------------------------------------------");
+        }
+
+        public override string ToString()
+        {
+            return $"Korisnik: {Ime} {Prezime}, Korisničko ime: {KorisnickoIme}, Prijavljen: {StatusPrijave}, Port: {DodeljeniPort}";
+        }
+    }
+}
